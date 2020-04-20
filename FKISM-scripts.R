7ee6440955ff68b5b0062f7310b6c7675f0be0f6
Tristan Louth-Robins <tristanlouthrobins@MacBook-Pro-4.local>
2020-04-20 15:33:17 +0930

    Fixed variables to make consistent with FKISM dataset.

--- FKISM-scripts.R
+++ FKISM-scripts.R
@@ -12,10 +12,10 @@ view(data)
 # Plot by location 2011-present
 
 new_data_loc <-
-  select(data, Location, Day, Month, Year, Environment, Quality) 
+  select(data, Location, Day, Month, year, Environment, Quality) 
 
-new_data_loc <- new_data_loc[ , c("Location",  "Day", "Year", "Month", "Environment", "Quality")] %>% 
-  unite(Date, Year, Month, Day, sep = "/", remove = TRUE, na.rm = FALSE) %>% 
+new_data_loc <- new_data_loc[ , c("Location",  "Day", "year", "Month", "Environment", "Quality")] %>% 
+  unite(Date, year, Month, Day, sep = "/", remove = TRUE, na.rm = FALSE) %>% 
   arrange(Date) 
 
 new_data_loc$Date <- as.Date(new_data_loc$Date,format="%Y/%m/%d")
@@ -32,10 +32,10 @@ view(new_data_loc)
 # Plot by region 2011-present
 
 new_data_reg <-
-  select(data, Region, Day, Month, Year, Environment, Quality) 
+  select(data, Region, Day, Month, year, Environment, Quality) 
 
-new_data_reg <- new_data_reg[ , c("Region",  "Day", "Year", "Month", "Environment", "Quality")] %>% 
-  unite(Date, Year, Month, Day, sep = "/", remove = TRUE, na.rm = FALSE) %>% 
+new_data_reg <- new_data_reg[ , c("Region",  "Day", "year", "Month", "Environment", "Quality")] %>% 
+  unite(Date, year, Month, Day, sep = "/", remove = TRUE, na.rm = FALSE) %>% 
   arrange(Date) 
 
 new_data_reg$Date <- as.Date(new_data_reg$Date,format="%Y/%m/%d")
@@ -48,5 +48,5 @@ ggplot(
 
 view(new_data_reg)
 
-#below comments
+
 
