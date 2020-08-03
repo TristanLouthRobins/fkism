fdf4fe2f087cd12272efc67a6ffce55c04bd176d
Tristan Louth-Robins <tristanlouthrobins@MacBook-Pro-4.local>
2020-08-03 15:58:14 +0930

    updated code to work with new naming conventions in dataset.

--- FKISM-scripts.R
+++ FKISM-scripts.R
@@ -4,37 +4,39 @@ library(tidyverse)
 library(purrr)
 library(lubridate)
 
-data <- read_csv("data/FKISM.csv")
+data <- read_csv("data/FKISM-2.csv")
 
 view(data)
 
 ##################
 # Plot by location 2011-present
 
-new_data_loc <-
-  select(data, Location, Day, Month, year, Environment, Quality) 
+data <-
+  select(data, Location, Day, Month, year, Enviro1, Quality) 
 
-new_data_loc <- new_data_loc[ , c("Location",  "Day", "year", "Month", "Environment", "Quality")] %>% 
+data_sel <- data[ , c("Location",  "Day", "year", "Month", "Enviro1", "Quality")] %>% 
   unite(Date, year, Month, Day, sep = "/", remove = TRUE, na.rm = FALSE) %>% 
   arrange(Date) 
 
-new_data_loc$Date <- as.Date(new_data_loc$Date,format="%Y/%m/%d")
+data_sel$Date <- as.Date(data_sel$Date,format="%Y/%m/%d")
+
+view(data_sel)
 
 ggplot(
-  data = new_data_loc,
-  mapping = aes(x = Date, y = Location, colour = Environment, size = Quality)
+  data = data_sel,
+  mapping = aes(x = Date, y = Location, colour = Enviro1, size = Quality)
 ) +
   geom_point(alpha = 0.5)
 
-view(new_data_loc)
+view(data_sel)
 
 ################
 # Plot by region 2011-present
 
 new_data_reg <-
-  select(data, Region, Day, Month, year, Environment, Quality) 
+  select(data, Region, Day, Month, year, Enviro1, Quality) 
 
-new_data_reg <- new_data_reg[ , c("Region",  "Day", "year", "Month", "Environment", "Quality")] %>% 
+new_data_reg <- new_data_reg[ , c("Region",  "Day", "year", "Month", "Enviro1", "Quality")] %>% 
   unite(Date, year, Month, Day, sep = "/", remove = TRUE, na.rm = FALSE) %>% 
   arrange(Date) 
 
@@ -42,7 +44,7 @@ new_data_reg$Date <- as.Date(new_data_reg$Date,format="%Y/%m/%d")
 
 ggplot(
   data = new_data_reg,
-  mapping = aes(x = Date, y = Region, colour = Environment, size = Quality)
+  mapping = aes(y = Date, x = Region, colour = Enviro1, size = Quality)
 ) +
   geom_point(alpha = 0.5)
 
